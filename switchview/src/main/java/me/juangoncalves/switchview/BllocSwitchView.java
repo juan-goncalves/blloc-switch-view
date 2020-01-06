8391ea241a0df03af81f06eef4fca264ccd3b266
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-06 19:12:50 -0400

    Fix typo

--- switchview/src/main/java/me/juangoncalves/switchview/BllocSwitchView.java
+++ switchview/src/main/java/me/juangoncalves/switchview/BllocSwitchView.java
@@ -390,7 +390,7 @@ public class BllocSwitchView extends View {
         @Override
         public void writeToParcel(Parcel out, int flags) {
             super.writeToParcel(out, flags);
-            out.writeInt(isChekedAsInt());
+            out.writeInt(isCheckedAsInt());
         }
 
         public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
@@ -403,7 +403,7 @@ public class BllocSwitchView extends View {
             }
         };
 
-        private int isChekedAsInt() {
+        private int isCheckedAsInt() {
             if (isChecked)
                 return TRUE;
             else
